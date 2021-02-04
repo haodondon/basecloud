2cc8d4819050123a7bd29d4e0a7f3c5173673ac1
haodd <15229396450@126.com>
2021-02-04 18:05:48 +0800

    用户服务sql

---
+++ basecloud-oauth/basecloud-user/sql/basecloud-user.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.32 : Database - basecloud-user
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`basecloud-user` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `basecloud-user`;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` varchar(50) NOT NULL,
+  `t_user_name` varchar(20) DEFAULT NULL COMMENT '用户名称',
+  `t_user_account` varchar(20) DEFAULT NULL COMMENT '用户登录账号',
+  `t_user_password` varchar(20) DEFAULT NULL COMMENT '用户登录密码',
+  `t_user_pic` varchar(500) DEFAULT NULL COMMENT '用户头像',
+  `t_user_frozen` int(1) DEFAULT NULL COMMENT '用户冻结状态 0-否 1-是',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `status` int(1) DEFAULT NULL COMMENT '是否删除 0-否 1-是',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
