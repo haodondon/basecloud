3a63d352074abef1b6b6b4d62f6f41a942b07963
haodd <15229396450@126.com>
2021-02-09 13:04:06 +0800

    完善oauth2认证流程

---
+++ basecloud-oauth/basecloud-oauth-gateway/src/main/java/cn/example/OauthGatewayApplication.java
@@ -0,0 +1,10 @@
+package cn.example;
+
+/**
+ * @author 一枚路过的程序猿
+ * @Title: 网关
+ * @date 2021/2/9 11:41
+ */
+@
+public class OauthGatewayApplication {
+}
---
+++ basecloud-oauth/basecloud-oauth-server/src/main/java/cn/example/controller/PublicKeyController.java
@@ -0,0 +1,9 @@
+package cn.example.controller;
+
+/**
+ * @author 一枚路过的程序猿
+ * @Title: 证书公钥
+ * @date 2021/2/9 11:45
+ */
+public class PublicKeyController {
+}
